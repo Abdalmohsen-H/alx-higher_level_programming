62391457301ffdc09eef34aed55f3b7339f1af93
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-06 16:10:08 -0700

    they requre string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for asciichar in range(97, 123):  # 97 to 122 ascii for a to z
-    print(chr(asciichar), end='')
+    print(f"{chr(asciichar)}", end='')
 
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for asciichar in range(ord('a'), ord('z')+1):
     if asciichar not in (101, 113):  # ascii 113 = q , 101 = e
-        print(chr(asciichar), end='')
+        print(f"{chr(asciichar)}", end='')
