6a4d0770748fb3cb30aabeaed5b290870361aed6
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-06 16:46:00 -0700

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for decimnum in range(0, 99):
     # print(f"{decimnum} = {hex(decimnum)}")  # hex() to convert to hexdecimal
-    print("{} = {}".format(decimnum, hex(decimnum)))  # hex() to convert to hexdecimal
+    print("{} = {}".format(decimnum, hex(decimnum)))
