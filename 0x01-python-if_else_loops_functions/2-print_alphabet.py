d3c1c585525f6c1d3a68aca3b9b6e56dd7454f57
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-06 16:15:57 -0700

    try to see why checker have wrong loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for asciichar in range(97, 123):  # 97 to 122 ascii for a to z
+for asciichar in range(97, 123):
+    # 97 to 122 ascii for a to z
     print("{}".format(chr(asciichar)), end='')
 
