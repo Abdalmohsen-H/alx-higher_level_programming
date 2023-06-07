2bbfa2ab80320386957d25e16cc1d7dbae8a6aaf
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-06 17:35:22 -0700

    task 101 handle negative n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    if n < 0:  # handle nagtive n index
+        n = len(str) + n
     return (str[:n] + str[n+1:])
 
 
-# remove_char_at("tset", 2)
+# remove_char_at("test", 2)
+# remove_char_at("Chicago", -3)
