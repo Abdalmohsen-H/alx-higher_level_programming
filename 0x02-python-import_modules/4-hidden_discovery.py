263e4254926a7d7fa984bcd6c507b7fdadb64b0e
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-08 15:21:21 -0700

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    definednames = dir(hidden_4)  # names defined by a module
+    definednames.sort()
+    for definedname in definednames:
+        if definedname[0] != "_":  # if_not started with _ 
+            print("{}".format(definedname))
