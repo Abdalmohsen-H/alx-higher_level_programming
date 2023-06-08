0325d1dde8a5f5fa72aeed736c61b3edeab3d1f3
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-08 15:23:18 -0700

    task 4 fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     definednames = dir(hidden_4)  # names defined by a module
     definednames.sort()
     for definedname in definednames:
-        if definedname[0] != "_":  # if_not started with _ 
+        if definedname[0] != "_" and definedname[1] != "_":
+            # if_not started with _ 
             print("{}".format(definedname))
