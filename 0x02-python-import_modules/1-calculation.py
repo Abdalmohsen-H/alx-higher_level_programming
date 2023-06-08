227c2f04b15edf16e743264db824fcdc888c9041
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-08 11:29:37 -0700

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-# import modules
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    # define vars
     a = 10
     b = 5
-    # print output
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
