d9ebef7f847b195c231123fe462d815e1325e314
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-14 07:59:03 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # task 7 : updae or add one item to dict
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+"""
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+"""
