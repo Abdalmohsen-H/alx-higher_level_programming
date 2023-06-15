03034d8d2d061d273726b1eece83c431eb8af195
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-14 18:51:56 -0700

    task 12 interview question , add type checking andNone checker

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     # task 12 interview question : roman to decimal converter
+    # if isinstance(roman_string, str):  # better for type checking 
+    if type(roman_string) == str or roman_string is None:
+        return 0
     total = 0
     convrt = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for indx, romchar in enumerate(roman_string):
