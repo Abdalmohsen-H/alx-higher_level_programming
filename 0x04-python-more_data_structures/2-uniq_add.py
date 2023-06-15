04ecdd1a1596b3dc0379bec28e3aa400a6cce99a
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-14 17:14:04 -0700

    task 2 fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,6 @@ def uniq_add(my_list=[]):
     # task no. 2
     #  return sum of unique elements of a list.
     sum = 0
-    if len(my_list) < 1:
-        return None
     for elem in set(my_list):  # use set to get unique elements
         sum = sum + elem
     return sum
