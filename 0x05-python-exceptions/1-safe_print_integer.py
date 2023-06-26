cd04b1d819f6ce5fcc9dbbf8d466cb8efc0d718c
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 09:22:39 -0700

    task 1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    # except Exception as err:
-    except (TypeError, ValueError):
+        return (True)
+    except Exception as err:
+        # except (TypeError, ValueError):
         # print(f"Unexpected {err=}, {type(err)=}")
-        return False
+        return (False)
