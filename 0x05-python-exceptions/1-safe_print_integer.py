d32cf91641a05208b0e7c16096747f6137ea44cf
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 09:21:05 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as err:
+    # except Exception as err:
+    except (TypeError, ValueError):
         # print(f"Unexpected {err=}, {type(err)=}")
         return False
