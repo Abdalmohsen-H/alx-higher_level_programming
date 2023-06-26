6e3c7c740cb03e2066198b0f83fb06c911c04d51
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 16:38:50 -0700

    task 6 + refactor 4 and 5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    """
+    task 4
+    A.Hesham
+    """
     nwlst = []
-    for idx in range(list_length):
+    for idx in range(list_length):  # Author : A.Hesham
         res = 0  # this line must be 1st line inside for loop
         try:
             res = my_list_1[idx] / my_list_2[idx]
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    # task 6
+    # func that only raise exception with an input msg
+    # A.Hesham
+    raise NameError(message)
