384e56104a35a3f6c574060e11123ca4251401ec
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 11:24:55 -0700

    task 4 fix 3

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,10 +4,6 @@ def list_division(my_list_1, my_list_2, list_length):
     for idx in range(list_length):
         res = 0  # this line must be 1st line inside for loop
         try:
-            if type(my_list_2[idx]) not in (int, float):
-                print("wrong type")
-            if type(my_list_1[idx]) not in (int, float):
-                print("wrong type")
             res = my_list_1[idx] / my_list_2[idx]
         except TypeError:
             print("wrong type")
