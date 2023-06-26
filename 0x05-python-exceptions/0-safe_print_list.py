ff5cd245106e709b09a06678a4e4b2b476860e0c
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 09:09:28 -0700

    task 0.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[idx]), end="")
             cntr += 1
         except Exception as err:
-            pass
+            break
     print()
     return (cntr)
