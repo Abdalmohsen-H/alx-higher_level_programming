7d1e4945249cbb96fef8bc18b8382f980652ae05
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 09:11:29 -0700

    task 0 fix.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     cntr = 0
     for idx in range(x):
         try:
             print("{}".format(my_list[idx]), end="")
             cntr += 1
-        except Exception as err:
+        # except Exception as err:
+        except IndexError:
+            # print(f"Unexpected {err=}, {type(err)=}")
             break
     print()
     return (cntr)
