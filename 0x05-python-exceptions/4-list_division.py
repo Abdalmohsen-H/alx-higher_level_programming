19d15b67cce6d9c12de20d5f03b9a4099953cbe5
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 11:15:05 -0700

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    nwlst = []
+    for idx in range(list_length):
+        res = 0  # this line must be 1st line inside for loop
+        try:
+            if type(my_list_2[idx]) not in (int, float):
+                print("wrong type")
+            if type(my_list_1[idx]) not in (int, float):
+                print("wrong type")
+            res = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except Exception:
+            pass
+        nwlst.append(res)
+    return nwlst
