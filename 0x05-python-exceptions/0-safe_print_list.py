7e364bd347e1aef99f4eeaecedf4921de77de4c4
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-26 09:05:04 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@ def safe_print_list(my_list=[], x=0):
     cntr = 0
     for idx in range(x):
         try:
-            print("{}".format(my_list[x]), end="")
+            print("{}".format(my_list[idx]), end="")
             cntr += 1
         except Exception as err:
             pass
