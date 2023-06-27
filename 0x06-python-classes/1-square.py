20ddb0667d4dbaeda5ffbaf267af9569c706dfaa
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-27 00:17:36 -0700

    task 1 class accept argument also have private attribute called size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square():
+    # task 1 , private attribut + class accept arguments
+    __size = None
+
+    def __init__(self, _Square__size=None):
+        self._Square__size = _Square__size
