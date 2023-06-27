d7f2bacee4018958edcf419e9e2d458565384002
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-27 11:53:01 -0700

    doc style fix task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,6 +10,7 @@ Square():
     """
 
     def __init__(self, size=0):
+        """ intialize instance """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -17,4 +18,7 @@ Square():
         self.__size = size
 
     def area(self):
+        """access another public method'instance attribute
+        size and calculate the area of square
+        """
         return (self.__size ** 2)
