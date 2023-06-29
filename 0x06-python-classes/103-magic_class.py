8662de8bcabd404e162111577be03e70f1c97f6a
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-28 18:38:22 -0700

    task 10 bytecode to python

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,9 +11,8 @@ class MagicClass:
 
     def __init__(self, radius=0):
         """ intialize circle instance"""
-
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
