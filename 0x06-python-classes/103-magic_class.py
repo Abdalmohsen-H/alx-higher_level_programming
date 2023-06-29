dad92ce56336e7cc443aea4585e003a2ea8d83c9
Abdalmohsen-H <egywebdes@gmail.com>
2023-06-28 18:35:33 -0700

    task 10 bytecode to python

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import math
+"""
+given bytecode .pyc I figured out the .py file
+python code for circle calulations
+"""
+
+
+class MagicClass:
+    """ circle class """
+
+    def __init__(self, radius=0):
+        """ intialize circle instance"""
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """ calculate area """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """ circle circumference (moheet) """
+        return 2 * math.pi * self.__radius
