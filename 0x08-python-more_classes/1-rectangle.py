53ba72858dc606e63751b5a045f15699bdd50b78
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-03 17:43:19 -0700

    tst task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         ''' intialize instance (object) of class
         Rectangle
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
