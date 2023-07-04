882c8cb28357ad1728cc8229e9088acce44942f7
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-03 17:11:48 -0700

    task 1 fix doc

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,19 +6,28 @@ A. Hesham
 
 
 class Rectangle:
-    ''' This is a Rectangle class '''
+    ''' This is a Rectangle class
+    ######################
+    '''
     def __init__(self, width=0, height=0):
+        ''' intialize instance (object) of class
+        Rectangle #######
+        '''
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        '''width  getter '''
+        '''width  getter
+        ######################
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        ''' width setter '''
+        ''' width setter
+        #################
+        '''
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -27,7 +36,9 @@ class Rectangle:
 
     @property
     def height(self):
-        '''height  getter '''
+        '''height  getter
+        #############
+        '''
         return self.__height
 
     @width.setter
