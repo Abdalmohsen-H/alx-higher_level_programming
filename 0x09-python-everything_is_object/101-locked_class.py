3f9f55ddca9cbc143e945f4fa6207c1a1712e6a9
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-04 19:42:00 -0700

    task 30 (101)

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+this a locked class that doesn't any
+instance attributes except specific attribute
+'''
+
+
+class LockedClass:
+    ''' this class doesn't have any attributes except specified
+    and have restriction conditions
+    __slots__ only accept squence like lists or tuples ...
+    '''
+
+    __slots__ = 'First_name'
