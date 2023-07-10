63da3161236f3f3402f6faa886d94d8c70ec452b
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-10 08:35:34 -0700

    task 4 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,6 +15,20 @@ def inherits_from(obj, a_class):
     fro_m the specified class
     return True or False
     '''
-    # Another solution
-    # return issubclass(obj.__class__, a_class)
-    return issubclass(type(obj), a_class)
+    # Another solutions:
+
+    # return issubclass(obj.__class__, a_class)\
+    # and (a_class != type(obj))
+
+    # return isinstance(obj, a_class) and (a_class != type(obj))
+
+    # return (a_class in list(type(obj).__mro__)\
+    #        and (a_class != type(obj)))
+
+    # return issubclass(type(obj), a_class) and (a_class != type(obj))
+
+    if (a_class == type(obj)):  # if oject is an instance of a_class
+        return False
+    else:  # then check if obj's class inheriting fro-m a_clas
+        # below line returns True if obj's class inherit fr.om a_class
+        return issubclass(type(obj), a_class)
