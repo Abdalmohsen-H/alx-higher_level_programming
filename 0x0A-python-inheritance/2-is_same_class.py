ce1e553720a219f328423810367e5a4e079db8af
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-10 03:57:51 -0700

    task 2'

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,17 +3,15 @@
 that takes an object & class as input
 Task 2 : A. Hesham
 returns True if the oject is
-an instance of the class
+an (exact) instance of the class
 else false
+the imporatnt keyword here is "exact instance"
 '''
 
 
 def is_same_class(obj, a_class):
     ''' a function that checks if object is
-    an instance of the class
+    an (exact) instance of the class
     return True or False
     '''
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
