3e8fdb614b718235844e22a109a9f18e66d6aa8b
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-10 14:06:24 -0700

    tasks 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' a module that have a class inherits Rectangle
+Task 10 : A. Hesham
+'''
+
+
+# BaseGeometry = __import__('9-rectangle').BaseGeometry
+
+
+class Square(Rectangle):
+    ''' rectangle class inherits
+    base geometery
+    '''
+    def __init__(self, size):
+        ''' intialize the object for this class '''
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        ''' override inherited area ()'''
+        return self.__size * self.__size
+
+    def __repr__(self):
+        ''' repr for str() and print() '''
+        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
