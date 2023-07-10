5b240dc3767f19556c98f4fd9a2b3799026639b9
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-10 09:32:36 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-''' a module that have an empty class
+''' a module that have a class inherits other
+class
 Task 8 : A. Hesham
 '''
 
 
 class Rectangle(BaseGeometry):
-    ''' rectangle class inherits from
+    ''' rectangle class inherits
     base geometery
     '''
     def __init__(self, width, height):
-        ''' intialize the object from this class '''
+        ''' intialize the object for this class '''
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
