ad2325f84657a2f6c08d636764d0731ea1bcaf9b
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-10 09:42:26 -0700

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' a module that have a class inherits other
+class
+Task 9 : A. Hesham
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' rectangle class inherits
+    base geometery
+    '''
+    def __init__(self, width, height):
+        ''' intialize the object for this class '''
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __repr__(self):
+        ''' repr for str() and print() '''
+        return f"[Rectangle] <{self.__width}>/<{self.__height}>"
+
+    def area(self):
+        ''' override inherited area ()'''
+        return self.__width * self.__height
