eb6c86488045430061ea65ca026890f8f3482dd7
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-11 10:48:41 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' a module that have a function
+returns JSON representation of an object
+    i.e. (returns string)
+'''
+
+
+def to_json_string(my_obj):
+    ''' functions returns JSON representation of an object
+    i.e. (returns string)
+    '''
+    return json.dumps(my_obj)
