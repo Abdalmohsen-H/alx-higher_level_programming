13119b209698a4e842208dbee2776bcb66888fd0
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-11 07:21:02 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' a module that have a function
+for reading a file with utf-8 encoding
+'''
+
+
+def read_file(filename=""):
+    ''' function to read file '''
+    if len(filename) > 0:
+        mode = "r"
+        with open(filename, mode, encoding="utf-8") as file:
+            for lines in file:
+                print(lines)
