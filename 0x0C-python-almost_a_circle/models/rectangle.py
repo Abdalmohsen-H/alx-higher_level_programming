8bf0c96cf249fefee17f24d73598ab159876bc4f
Abdalmohsen-H <egywebdes@gmail.com>
2023-07-15 15:45:30 -0700

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,14 @@ class Rectangle(Base):
         ''' repr for str() and print() '''
         return f"[{self.__class__.__name__}] ({self.id}) \
 {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        ''' update attibutes using args '''
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except Exception as e:
+            pass
