abcbc5d717b7cf5c0a513a20ca36b058dd150134
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-16 14:53:32 -0700

    show permissions for  users, also create users with privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- task 0 : script to show all privileges of specific MySQL user on Mysql server (in localhost).
+-- task 0 : script to show all privileges of specific MySQL user
+-- on Mysql server in localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
