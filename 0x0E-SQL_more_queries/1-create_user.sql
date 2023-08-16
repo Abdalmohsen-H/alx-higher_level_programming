319cc8dc1fd386ecc360876118ff974874fd5344
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-16 15:05:28 -0700

    task 1 & 3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- task 1: create user for MySQL server, user_0d_1, with pwd user_0d_1_pwd
 -- then give him all permissions(PRIVILEGES)
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
