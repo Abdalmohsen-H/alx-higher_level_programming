d841852ca8b9637126322c0f0e56f4661254575f
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-12 10:11:02 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // like task 1 but using array and a loop
-myValues = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const myValues = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 // myValues.forEach((value, index)=>{
 // console.log(value);
 // });
-for (let index = 0; index < myValues.length; index++){
-    console.log(myValues[index]);
+for (let index = 0; index < myValues.length; index++) {
+  console.log(myValues[index]);
 }
