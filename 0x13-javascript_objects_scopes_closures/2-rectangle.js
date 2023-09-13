38e1870b70a1936a581c0c3f88908c881f1f345a
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-12 19:08:01 -0700

    task 3'

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 // task 1: create rectangle class with constructor
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 ){
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 };
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,14 +2,14 @@
 // task 1: create rectangle class with constructor
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0 ){
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 
-  print(){
-    for ( let vunit = 0; vunit < this.height; vunit++){
+  print () {
+    for (let vunit = 0; vunit < this.height; vunit++) {
       console.log('X'.repeat(this.width));
     }
   }
