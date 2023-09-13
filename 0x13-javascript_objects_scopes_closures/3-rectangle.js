0846019946638a7238aa053b01df1595baadeb7f
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-12 19:05:21 -0700

    task 3'

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// task 1: create rectangle class with constructor
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 ){
+    this.width = w;
+    this.height = h;
+    }
+  }
+
+  print(){
+    for ( let vunit = 0; vunit < this.height; vunit++){
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
