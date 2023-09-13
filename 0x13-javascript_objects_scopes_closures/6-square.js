15fe567f791e6e7eec67b114e182c7ad2ffee535
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-12 19:44:27 -0700

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+ * inheritance in javascipt oop using extends
+ * also we will import the parent class using require(filepath)
+*/
+module.exports = class Square extends require('./5-square.js') {
+  charPrint(c) {
+    // draw square with user defined char , otherwise use default X
+    if (c === undefined){
+      this.print();
+    } else {
+      for (let vunit = 0; vunit < this.height; vunit++) {
+        console.log(c.repeat(this.width));
+    }
+
+    }
+  }
+};
