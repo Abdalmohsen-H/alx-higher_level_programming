ae4cee5516b21ae555761d32c0188bcff3557234
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-12 19:46:50 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,15 +4,14 @@
  * also we will import the parent class using require(filepath)
 */
 module.exports = class Square extends require('./5-square.js') {
-  charPrint(c) {
+  charPrint (c) {
     // draw square with user defined char , otherwise use default X
-    if (c === undefined){
+    if (c === undefined) {
       this.print();
     } else {
       for (let vunit = 0; vunit < this.height; vunit++) {
         console.log(c.repeat(this.width));
-    }
-
+      }
     }
   }
 };
