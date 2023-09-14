d93331e2dfa246708529d761c5b41de8307d40b9
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-13 17:55:35 -0700

    exports a named function

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// task 13 function to add 2 inputs
+exports.add = function add (a, b) {
+  return (a + b);
+};
+// no need to name the function is it already named in assign
+// so the following will work the same
+// export.add = function (a, b){
+//   return (a + b);
+// }
