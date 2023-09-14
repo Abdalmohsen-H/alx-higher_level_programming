ff048cef8a24f016454104411363ebfac4e4df77
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-13 17:39:29 -0700

    task 8 - print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// task 8 print square
+const sizeArg = process.argv[2]
+if (sizeArg === undefined){
+  console.log('Missing size');
+} else {
+  const size = parseInt(sizeArg, 10);
+  if (isNaN(size)) {
+    console.log('Missing size');
+  } else {
+    for (let vunit = 0; vunit < size; vunit++){
+        console.log('X'.repeat(size));
+    }
+  }
+}
