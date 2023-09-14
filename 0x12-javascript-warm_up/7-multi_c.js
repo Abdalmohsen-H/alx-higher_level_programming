dae16101f02caaa36373c5ff1e5290f8f1485900
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-13 17:42:41 -0700

    task 8 - print square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,14 +4,14 @@
 * user will enter required number of printing as argument
 * handle cases when no argument or argument isn't a number
 */
-let args = process.argv
-if (args[2] === 'undefined'){
-    console.log('Missing number of occurrences');
+const args = process.argv;
+if (args[2] === 'undefined') {
+  console.log('Missing number of occurrences');
 }
-let num = parseInt(args[2], 10);
-if (isNaN(num)){
-    console.log('Missing number of occurrences');
+const num = parseInt(args[2], 10);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
 }
-for (let count = 0; count < num; count++){
-    console.log('C is fun');
+for (let count = 0; count < num; count++) {
+  console.log('C is fun');
 }
