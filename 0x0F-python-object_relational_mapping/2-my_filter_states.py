862e26f9dadb431a88ab287138e397f0b8a469be
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-15 17:46:04 -0700

    task 2 : Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" script that lists all states from the database
+with a name starting with N (upper N)"""
+from sys import argv
+import MySQLdb
+
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': argv[1],
+    'passwd': argv[2],
+    'db': argv[3],
+    'charset': 'utf8'
+}
+
+if __name__ == "__main__":
+    db_connection = MySQLdb.connect(**DB_CONFIG)
+    cur = db_connection.cursor()
+    try:
+        sql_cmd = """
+        SELECT * FROM states
+        WHERE name LIKE BINARY '{:s}'
+        ORDER BY id ASC
+        """.format(argv[4])
+
+        cur.execute(sql_cmd)
+        result = cur.fetchall()
+
+        for record in result:
+            print(record)
+
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: {}".format(str(e)))
+
+    cur.close()
+    db_connection.close()
