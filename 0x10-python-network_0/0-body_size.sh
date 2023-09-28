a4b5bcebc51d6576f43f7a7846ec605703189139
Your Name <you@example.com>
2023-09-28 14:20:19 -0700

    task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# task 0: Bash script to take a URL from user as argument -> "$1", Then
-# send a request to this URL -> curl -s "$1" # s option means silent
-# then show size of response body in bites -> | wc -c
+# task 0: Bash script to take a URL from user "$1", Then send a request, s option means silent;then show size of response body in bites |wc -c
 curl -s "$1" | wc -c
