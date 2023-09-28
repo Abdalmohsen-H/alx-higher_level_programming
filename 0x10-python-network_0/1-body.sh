63af0037d6db12384ac3e0ca84d317b484d05411
Your Name <a.hesham@gr8dev.tech>
2023-09-28 14:22:59 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # return response body for a request on a given URL only if status code == 200; also follow redirects with option L
-curl -s -o /dev/null -w "%{http_code}\n" -L "$1" | ( read -r status; [ "$status" -eq 200 ] && curl -s -L "$1" || : )
+curl -s -o /dev/null -w "%{http_code}\n" -L "$1" | ( read -r status; [ "$status" -eq 200 ] && curl -s -L "$1" )
