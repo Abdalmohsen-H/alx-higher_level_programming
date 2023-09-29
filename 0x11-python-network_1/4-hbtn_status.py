d79d3bd3da1b1307ca198b7c724107ac4b48d8ee
Your Name <a.hesham@gr8dev.tech>
2023-09-29 15:28:26 -0700

    task 4 : requests package det url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""task 4:  Py script to show how to fetche urls
+using requests package"""
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print(f"\t- type: {type(res.text)}")  # str
+    #print(f"\t- type: {type(res.content)}")  # bytes
+    print("\t- content: {}".format(res.text))
