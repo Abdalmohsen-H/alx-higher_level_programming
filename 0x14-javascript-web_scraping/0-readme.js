0f291ef81654ecbcf8f05a5d346aee73bc16237d
mohsen <egywe.bdes@gmail.com>
2023-10-24 09:33:26 -0700

    fix semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,11 @@
 // import fs module (file system module)
 const fs = require('fs');
 // assign first argument to file_path
-const file_path = process.argv[2];
+const filePath = process.argv[2];
 // this could be done on different ways, ref: https://nodejs.dev/en/learn/reading-files-with-nodejs/
-try{
-	const rdata = fs.readFileSync(file_path, 'utf8');
-	console.log(rdata);
-} catch(err) {
-	console.log(err);
-};
-
+try {
+  const rdata = fs.readFileSync(filePath, 'utf8');
+  console.log(rdata);
+} catch (err) {
+  console.log(err);
+}
