4eef4329b810072c49cd9f68be5d22e2c366e531
mohsen <egywe.bdes@gmail.com>
2023-10-24 17:43:14 -0700

    task 4 : counting phrase occurrences in stringified JSON data.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 // import fs module (file system module)
 const request = require('request');
-// assign first argument to baseUrl
-const baseUrl = process.argv[2];
-const charcId = 18
-const filter = 'people'
-const url = baseUrl + '?' + filter + '=' + charcId
+// assign first argument to url
+const url = process.argv[2];
+const searchPherase = 'api/people/18';
 
 request.get(url, (error, response) => {
-	if (error) {
-		console.log(error);
-	}
-	console.log(JSON.parse(response.body).count);
+  if (error) {
+    console.log(error);
+  }
+  const count = (response.body.split(searchPherase).length) - 1;
+  console.log(count);
 });
